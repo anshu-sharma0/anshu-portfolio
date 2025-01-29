1826abd9f2501202dd1973a00eb437b47618658a
Akshat <ashu.dnn@gmail.com>
2025-01-30 00:25:49 +0530

    gap experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,7 +1,7 @@
 import { Wand2, Brain } from 'lucide-react'
 const Experience = () => {
     return (
-        <section className="py-20">
+        <section className="pb-20">
             <div className="container mx-auto px-6">
                 <h2 className="text-3xl font-bold mb-12 text-center scroll-animate">
                     Experience
